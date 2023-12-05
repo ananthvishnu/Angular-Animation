d6c385e38947eedbe1ea2c1ff5f214e6f0cdfe90
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-12-05 15:22:47 +0530

    menu added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FactComponent } from './components/fact/fact.component';
+import { ServiceComponent } from './components/service/service.component';
+import { EventsComponent } from './components/events/events.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { FactComponent } from './components/fact/fact.component';
     NavbarComponent,
     HeroComponent,
     AboutUsComponent,
-    FactComponent
+    FactComponent,
+    ServiceComponent,
+    EventsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
